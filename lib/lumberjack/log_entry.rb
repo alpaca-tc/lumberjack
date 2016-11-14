52c358195f998a5a81990186789f99769abda193
alpaca-tc <alpaca-tc@alpaca.tc>
2016-11-14 19:45:42 +0900

    Fix a warning in ruby 2.4.0-preview
    Ruby 2.4 unifies Fixnum and Bignum into Integer: https://bugs.ruby-lang.org/issues/12005

--- lib/lumberjack/log_entry.rb
+++ lib/lumberjack/log_entry.rb
@@ -3,22 +3,22 @@ module Lumberjack
   # information about the system that logged the message.
   class LogEntry
     attr_accessor :time, :message, :severity, :progname, :pid, :unit_of_work_id
-    
+
     TIME_FORMAT = "%Y-%m-%dT%H:%M:%S".freeze
-    
+
     def initialize(time, severity, message, progname, pid, unit_of_work_id)
       @time = time
-      @severity = (severity.is_a?(Fixnum) ? severity : Severity.label_to_level(severity))
+      @severity = (severity.is_a?(Integer) ? severity : Severity.label_to_level(severity))
       @message = message
       @progname = progname
       @pid = pid
       @unit_of_work_id = unit_of_work_id
     end
-    
+
     def severity_label
       Severity.level_to_label(severity)
     end
-    
+
     def to_s
       buf = "[#{time.strftime(TIME_FORMAT)}.#{(time.usec / 1000.0).round.to_s.rjust(3, '0')} #{severity_label} #{progname}(#{pid})"
       if unit_of_work_id
@@ -28,7 +28,7 @@ module Lumberjack
       buf << "] "
       buf << message
     end
-    
+
     def inspect
       to_s
     end
